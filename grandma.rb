3b9f4fc5c0fa663918e3ca7be1fb2ad8567f9eb9
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-08 13:17:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(statement)
   if statment == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
     elsif statement == statment.upcase
     return "NO, NOT SINCE 1938!"
   else
