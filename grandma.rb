4b5523f50d4aac090d060154188608d033ad04dc
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-08 13:19:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def speak_to_grandma(statement)
   if statement == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+    elsif statement == statement.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
 
